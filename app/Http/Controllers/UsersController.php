16a49452b5df4e8d095a776303579b47ba8d6f4a
马坤 <1191544503@qq.com>
2018-02-05 15:06:49 +0800

    finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\User;
 
 class UsersController extends Controller
 {
     //
     public function create(){
         return view('users.create');
+    }
+    public function show(User $user){
+        return view('users.show',compact('user'));
+    }
+    public function store(Request $request){
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'email'=>'email|required|max:255|unique:users',
+            'password'=>'required|confirmed|min:6',
+        ]);
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+        ]);
+        session()->flash('success','注册成功，欢迎');
+        return redirect()->route('users.show',[$user]);
     }
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,12 +1,13 @@
 <html>
 <head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="/weibo/public/css/app.css">
 </head>
 <body>
 @include('layouts._header')
 
 <div class="container">
+    @include('shared._message')
     @yield('content')
     @include('layouts._footer')
 </div>
