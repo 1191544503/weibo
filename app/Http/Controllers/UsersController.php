a9d622010817875b7698205ac9f3aab0e4091996
马坤 <1191544503@qq.com>
2018-02-07 11:24:07 +0800

    finish user edit update index and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,16 @@ use App\User;
 class UsersController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except'=>['create','store','index','show'],
+        ]);
+        $this->middleware('guest',[
+            'only'=>['create'],
+        ]);
+}
+
     public function create(){
         return view('users.create');
     }
@@ -29,5 +39,36 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success','注册成功，欢迎');//闪存
         return redirect()->route('users.show',[$user]);
+    }
+    public function edit(User $user){
+        $this->authorize('update',$user);
+        return view('users.edit',compact('user'));
+    }
+    public function update(User $user,Request $request){
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'nullable|confirmed|min:6',
+        ]);
+        $this->authorize('update',$user);
+        $data=[];
+        $data['name']=$request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+        session()->flash('success','更新成功!');
+        return redirect()->route('users.show',$user->id);
+    }
+
+    public function index(){
+        $users=User::paginate(10);
+        return view('users.index',compact('users'));
+    }
+
+    public function destroy(User $user){
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success','成功删除');
+        return back();
     }
 }
